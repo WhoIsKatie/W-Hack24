e20af60df2115100c34e1e54e6e2be433895e9b8
konomea <elsako902@gmail.com>
2024-03-02 12:30:54 -0500

    Trying to get messages from gmail api

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ from flask import Flask, session, request, redirect, render_template
 import google_auth_oauthlib.flow
 from app import util
 import secrets, os
+import requests
 
 app = Flask(__name__)
 app.secret_key = secrets.token_hex(16)
@@ -10,8 +11,8 @@ app.secret_key = secrets.token_hex(16)
 flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
     'client_secret.json',
     scopes=['https://www.googleapis.com/auth/gmail.readonly'])
-
 flow.redirect_uri = 'https://127.0.0.1:5000/oauth2callback'
+
 
 @app.route('/')
 def start():
@@ -33,9 +34,24 @@ def receive_voice():
 
 @app.route('/oauth2callback')
 def oauth2callback():
-    # authorization_response = request.url
-    # flow.fetch_token(authorization_response=authorization_response)
-    # session['credentials'] = flow.credentials
+    userId = 'me'
+    # Get the authorization code from the callback URL
+    authorization_response = request.url
+    # Exchange the authorization code for an access token
+    flow.fetch_token(authorization_response=authorization_response)
+    # Access token is now available in flow.credentials
+    access_token = flow.credentials.token
+
+    url = f'https://gmail.googleapis.com/gmail/v1/users/{userId}/messages'
+    headers = {'Authorization': f'Bearer {access_token}'}
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        messages = response.json()
+        # Process the retrieved messages
+    else:
+        print(f'Error: {response.status_code} - {response.text}')
     return redirect('/index')
 
 @app.route('/index')
