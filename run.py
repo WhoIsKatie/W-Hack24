936e36619c33c5ff3ca08efce5f076f9ab015a0c
konomea <elsako902@gmail.com>
2024-03-02 13:30:32 -0500

    Got email_list

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ import google_auth_oauthlib.flow
 from app import util
 import secrets, os
 import requests
+import json
 
 app = Flask(__name__)
 app.secret_key = secrets.token_hex(16)
@@ -12,6 +13,8 @@ flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
     'client_secret.json',
     scopes=['https://www.googleapis.com/auth/gmail.readonly'])
 flow.redirect_uri = 'https://127.0.0.1:5000/oauth2callback'
+
+email_list = []
 
 
 @app.route('/')
@@ -41,22 +44,31 @@ def oauth2callback():
     flow.fetch_token(authorization_response=authorization_response)
     # Access token is now available in flow.credentials
     access_token = flow.credentials.token
-
-    url = f'https://gmail.googleapis.com/gmail/v1/users/{userId}/messages'
     headers = {'Authorization': f'Bearer {access_token}'}
+    
+    # TODO: not sure if we want to keep the stuff
+    # with open('access_token.json', 'w') as f:
+    #     json.dump({'access_token': access_token, 'headers': headers}, f)
 
-    response = requests.get(url, headers=headers)
-
-    if response.status_code == 200:
-        messages = response.json()
-        # Process the retrieved messages
-    else:
+    all_messages_url = f'https://gmail.googleapis.com/gmail/v1/users/{userId}/messages'
+    response = requests.get(all_messages_url, headers=headers)
+    print(response.json())
+    
+    if response.status_code != 200:
         print(f'Error: {response.status_code} - {response.text}')
+    
+    for message in response.json()['messages']:
+        message_id = message['id']
+        message_url = f'https://gmail.googleapis.com/gmail/v1/users/{userId}/messages/{message_id}'
+        message_response = requests.get(message_url, headers=headers)
+        if message_response.status_code == 200:
+            message_data = message_response.json()
+            email_list.append(message_data['payload']['headers'])
     return redirect('/index')
 
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', email_list=email_list)
 
 
 if __name__ == '__main__':
