387b66cdd4d7768534efacf48a15d2c53989235f
konomea <elsako902@gmail.com>
2024-03-02 11:21:32 -0500

    google consent

--- app/util.py
+++ app/util.py
@@ -2,10 +2,17 @@ from flask import request
 
 from run import app
 
+def ml_analysis(text_data):
+    pass
+    # Placeholder code for processing voice data to text
+    # text = convert_to_text(voice_data)
+    # Send text to ML model for processing
+    # processed_text = ml_model.process(text)
+    # return processed_text
+    
+    # just assuming this returns array, [0] for the classification, [1] for the confidence
+
 def process_voice(voice_data):
-        pass
-        # Placeholder code for processing voice data to text
-        # text = convert_to_text(voice_data)
-        # Send text to ML model for processing
-        # processed_text = ml_model.process(text)
-        # return processed_text
+    pass
+    # placeholder
+    # assume this returns text data from voice files
--- run.py
+++ run.py
@@ -1,7 +1,25 @@
-from flask import Flask, render_template, request
+from flask import Flask, session, request, redirect, render_template
+import google_auth_oauthlib.flow
 from app import util
+import secrets, os
 
 app = Flask(__name__)
+app.secret_key = secrets.token_hex(16)
+
+
+flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+    'client_secret.json',
+    scopes=['https://www.googleapis.com/auth/gmail.readonly'])
+
+flow.redirect_uri = 'https://127.0.0.1:5000/oauth2callback'
+
+@app.route('/')
+def start():
+    # Redirect to Google authorization URL
+    authorization_url, state = flow.authorization_url(
+        access_type='offline',
+        prompt='consent')
+    return redirect(authorization_url)
 
 # Route for receiving voice data
 @app.route('/voice', methods=['POST'])
@@ -13,11 +31,17 @@ def receive_voice():
     processed_text = util.process_voice(voice_data)
     return processed_text
 
-# Route for serving web page
-@app.route('/')
+@app.route('/oauth2callback')
+def oauth2callback():
+    # authorization_response = request.url
+    # flow.fetch_token(authorization_response=authorization_response)
+    # session['credentials'] = flow.credentials
+    return redirect('/index')
+
+@app.route('/index')
 def index():
-    # Render HTML template
     return render_template('index.html')
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, ssl_context=('server.crt', 'server.key'))
